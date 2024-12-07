87a2a2e5a7212918d7e71d592fc9692b6c6adfc3
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-07 00:43:39 -0500

    chore: setup gh-pages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
-
+import parser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: parser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+    },
+  },
 ];
