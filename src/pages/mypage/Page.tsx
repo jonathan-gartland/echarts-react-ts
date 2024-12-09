ec349edd40952cf505ce305f27ec1d6b755313ee
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-09 11:33:12 -0500

    refactor: trying to fix pages deploy

--- src/pages/mypage/Page.tsx
+++ src/pages/mypage/Page.tsx
@@ -1,32 +1,29 @@
 import React from "react";
-import AgeVerification from "../../components/AgeVerification";
 import Jersey from "./jersey/Page";
 
 const Page: React.FC = () => {
   return (
     <div>
-      <AgeVerification>
+      <div>
         <div>
-          <div>
-            <div style={{ padding: 30, position: "relative" }}>
-              <img
-                className="h-96 w-full rounded-lg object-cover object-center filter contrast-[30%]"
-                // src={`${process.env.PUBLIC_URL}/2022-Santa-Cruz-Chameleon-Review_12.jpg`}
-                src={`${process.env.PUBLIC_URL}/fakelogo.png`}
-                alt="Fake Bourbon Logo"
-              />
-              {/*<h1 className=" mt-8 text-blue-100" style={{ fontSize: 196,  position: 'absolute', right: 50,  left: 50, top: 60}}>Jonny's Bikes</h1>*/}
-            </div>
+          <div style={{ padding: 30, position: "relative" }}>
+            <img
+              className="h-96 w-full rounded-lg object-cover object-center filter contrast-[30%]"
+              // src={`${process.env.PUBLIC_URL}/2022-Santa-Cruz-Chameleon-Review_12.jpg`}
+              src={`echarts-react-ts/fakelogo.png`}
+              alt="Fake Bourbon Logo"
+            />
+            {/*<h1 className=" mt-8 text-blue-100" style={{ fontSize: 196,  position: 'absolute', right: 50,  left: 50, top: 60}}>Jonny's Bikes</h1>*/}
           </div>
-          <h1></h1>
-          <h2>
-            Using Rugby Jerseys as stand in for alcohol. A tour through some of
-            my vintage rugby jerseys and the Ulster one as well.
-          </h2>
-          <Jersey />
-          <div style={{ width: "600px", height: "400px" }}></div>
         </div>
-      </AgeVerification>
+        <h1></h1>
+        <h2>
+          Using Rugby Jerseys as stand in for alcohol. A tour through some of my
+          vintage rugby jerseys and the Ulster one as well.
+        </h2>
+        <Jersey />
+        <div style={{ width: "600px", height: "400px" }}></div>
+      </div>
     </div>
   );
 };
